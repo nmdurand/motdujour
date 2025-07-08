007b1fe1702f12dd43b035b93c31b31eb4b46c13
Nicolas Durand <nicolas.durand@didask.com>
2025-07-08 16:09:01 +0200

    Displays letter states on the keyboard

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,4 +1,9 @@
 import { useGameContext } from "@/contexts/gameContext";
+import {
+  LetterState,
+  useKeyboardLetterState,
+} from "@/hooks/useKeyboardLetterState";
+import { twMerge } from "tailwind-merge";
 
 const KEYS_LINES = [
   ["a", "z", "e", "r", "t", "y", "u", "i", "o", "p"],
@@ -7,23 +12,21 @@ const KEYS_LINES = [
 ];
 
 export function Keyboard() {
+  const { word } = useGameContext();
   return (
-    <div className="flex flex-col gap-1">
-      {KEYS_LINES.map((line, index) => (
-        <KeyboardLine key={index} line={line} />
-      ))}
-    </div>
+    <>
+      {word}
+      <div className="flex flex-col gap-1">
+        {KEYS_LINES.map((line, index) => (
+          <KeyboardLine key={index} line={line} />
+        ))}
+      </div>
+    </>
   );
 }
 
 function KeyboardLine({ line }: { line: string[] }) {
   const { onLetter, onBackspace, onEnter } = useGameContext();
-
-  const getDisplayedLetter = (letter: string) => {
-    if (letter === "enter") return "↵";
-    if (letter === "backspace") return "⌫";
-    return letter.toUpperCase();
-  };
 
   const handleLetterClick = (letter: string) => {
     switch (letter) {
@@ -41,16 +44,50 @@ function KeyboardLine({ line }: { line: string[] }) {
   return (
     <div className="flex justify-center gap-1">
       {line.map((letter) => (
-        <button
+        <KeyboardLetter
           key={letter}
-          className="w-8 h-10 grow bg-gray-500 rounded-md
-          text-white text-2xl font-bold
-          hover:bg-gray-600 active:bg-gray-700 transition-colors cursor-pointer"
+          letter={letter}
           onClick={() => handleLetterClick(letter)}
-        >
-          {getDisplayedLetter(letter)}
-        </button>
+        />
       ))}
     </div>
+  );
+}
+
+function KeyboardLetter({
+  letter,
+  onClick,
+}: {
+  letter: string;
+  onClick: () => void;
+}) {
+  const letterState = useKeyboardLetterState({ letter });
+
+  const getDisplayedLetter = (letter: string) => {
+    if (letter === "enter") return "↵";
+    if (letter === "backspace") return "⌫";
+    return letter.toUpperCase();
+  };
+
+  const getLetterClasses = (letterState: LetterState | undefined) => {
+    if (letterState === "correct") return "bg-green-500";
+    if (letterState === "present") return "bg-yellow-500";
+    if (letterState === "incorrect") return "bg-gray-800";
+    return "bg-gray-500";
+  };
+
+  return (
+    <button
+      key={letter}
+      className={twMerge(
+        "w-8 h-10 grow rounded-md",
+        "text-white text-2xl font-bold",
+        "hover:opacity-80 active:opacity-60 transition-opacity cursor-pointer",
+        getLetterClasses(letterState)
+      )}
+      onClick={onClick}
+    >
+      {getDisplayedLetter(letter)}
+    </button>
   );
 }
---
+++ src/hooks/useKeyboardLetterState.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useMemo, useState } from "react";
+import { useGameContext } from "../contexts/gameContext";
+
+export type LetterState = "correct" | "incorrect" | "present";
+
+export function useKeyboardLetterState({
+  letter,
+}: {
+  letter: string;
+}): LetterState | undefined {
+  const [letterState, setLetterState] = useState<LetterState | undefined>(
+    undefined
+  );
+
+  const { guessedWords, word, currentGuessIndex } = useGameContext();
+
+  const guessedWordsToCheck = useMemo(
+    () => guessedWords.slice(0, currentGuessIndex),
+    [guessedWords, currentGuessIndex]
+  );
+
+  useEffect(() => {
+    const computeLetterState = (letter: string): LetterState | undefined => {
+      if (letter.length > 1) {
+        return undefined;
+      }
+      if (word === null) return undefined;
+      if (word.includes(letter)) {
+        for (let i = 0; i < word.length; i++) {
+          // letter is present in word to find
+          if (word[i] === letter) {
+            // for each index it appears at
+            // check if it appears in the guessedWords at the same index
+            // if it appears at the same index, return correct
+            for (let j = 0; j < guessedWordsToCheck.length; j++) {
+              if (guessedWordsToCheck[j][i] === letter) {
+                return "correct";
+              }
+            }
+          }
+        }
+        // else check if it appears at all in the guessedWords
+        // if it appears, return present
+        for (let j = 0; j < guessedWordsToCheck.length; j++) {
+          if (guessedWordsToCheck[j].includes(letter)) {
+            return "present";
+          }
+        }
+        // if it doesn't appear at all, return undefined
+        return undefined;
+      } else {
+        // letter is not present in word to find
+        // check if it appears in the guessedWords
+        // if it appears, return incorrect
+        for (let j = 0; j < guessedWordsToCheck.length; j++) {
+          if (guessedWordsToCheck[j].includes(letter)) {
+            return "incorrect";
+          }
+        }
+      }
+      return undefined;
+    };
+
+    setLetterState(() => computeLetterState(letter));
+  }, [letter, guessedWordsToCheck, word]);
+
+  return letterState;
+}
